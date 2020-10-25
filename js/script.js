9b272a6841313f9fd881fb709b091f3b4fcbdf4e
Nick Pino <you@example.com>
2020-10-25 16:38:20 +0300

    add scroll

--- js/script.js
+++ js/script.js
@@ -18,3 +18,15 @@ window.addEventListener('scroll', function () {
 		header.classList.remove("show")
 	}
 });
+//----------------прокрутука до блока---------------
+const anchors = document.querySelectorAll('a[href*="#"]')
+for (let anchor of anchors) {
+	anchor.addEventListener("click", function (e) {
+		e.preventDefault();
+		const blockID = anchor.getAttribute('href');
+		document.querySelector('' + blockID).scrollIntoView({
+			behavior: "smooth",
+			block: "nearest"
+		})
+	})
+}
